03a0f6e552a5b336dd4369118fcc44725d0fff81
Omer Dolev <omerdolev90@gmail.com>
2025-01-01 21:08:53 +0200

    hello typescript

---
+++ module-ii/01-hello-js/dist/app.js
@@ -0,0 +1,40 @@
+// alert("hello world");
+// alert("my name is Omer");
+// alert("I'm 34 years old");
+// alert("🥳🙏🔎");
+// alert(50);
+// alert(0.35);
+// alert("12");
+/* hello
+this is a comment
+with multiple lines */
+// alert(5 + 2);
+// alert(5 - 2);
+// alert(5 * 2);
+// alert(5 / 2);
+// alert(1 + 2 * 3 - 4);
+// alert("1" + "2");
+// alert("12");
+// alert("5" + 3);
+// alert(5 + "3");
+// alert("Your number is: " + 3);
+// alert("5" - 3);
+// Values - has types
+// Types: string, number
+// Operators: +, -, *, /
+// + - can be either addition or concatenation (depends on the operands)
+// const userName = prompt("What is your name?");
+// // const userName = "Omer";
+// alert("Hello " + userName);
+// // Exercise #1
+// // Input: "pizza"
+// // Output: "wow! pizza is my favorite to!"
+// const favoriteFood = prompt("What is your favorite food?");
+// alert("Wow! " + favoriteFood + " is my favorite to!");
+// Exercise #2
+// Input 1: 5
+// Input 2: 10
+// Output: 15
+var firstNumber = Number(prompt("Enter first number"));
+var secondNumber = Number(prompt("Enter second number"));
+alert(firstNumber + secondNumber);
