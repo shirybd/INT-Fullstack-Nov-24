0a9cea75380e5f0d0a01913dc7c8cc6b949985a9
Omer Dolev <omerdolev90@gmail.com>
2025-01-05 20:45:13 +0200

    if else if, logic operators

--- module-ii/02-05012025-conditions/dist/tallEnoughToRide.js
+++ module-ii/02-05012025-conditions/dist/tallEnoughToRide.js
@@ -5,3 +5,28 @@ var userHeightInCm = Number(prompt("What is your height? (cm)"));
 // At least 140cm tall
 // Or above 18 y/o
 // Output: "You can board the ride!" or "Go home"
+var isOlderThan8 = userAge > 8;
+var isAtLeast140Cm = userHeightInCm >= 140;
+var isOlderThan18 = userAge > 18;
+if (isOlderThan18) {
+    alert("You can board the ride!");
+}
+else if (!isOlderThan8) {
+    alert("Go home");
+}
+else if (isAtLeast140Cm) {
+    alert("You can board the ride!");
+}
+else {
+    alert("Go home");
+}
+// logic operators - takes 2 booleans and return a boolean
+//  && (and)
+// || (inclusive or)
+// Short circuit logic
+if (isOlderThan18 || (isOlderThan8 && isAtLeast140Cm)) {
+    alert("You can board the ride!");
+}
+else {
+    alert("Go home");
+}
--- module-ii/02-05012025-conditions/tallEnoughToRide.ts
+++ module-ii/02-05012025-conditions/tallEnoughToRide.ts
@@ -7,3 +7,28 @@ const userHeightInCm = Number(prompt("What is your height? (cm)"));
 // Or above 18 y/o
 
 // Output: "You can board the ride!" or "Go home"
+
+const isOlderThan8 = userAge > 8;
+const isAtLeast140Cm = userHeightInCm >= 140;
+const isOlderThan18 = userAge > 18;
+
+if (isOlderThan18) {
+    alert("You can board the ride!");
+} else if (!isOlderThan8) {
+    alert("Go home");
+} else if (isAtLeast140Cm) {
+    alert("You can board the ride!");
+} else {
+    alert("Go home");
+}
+
+// logic operators - takes 2 booleans and return a boolean
+//  && (and)
+// || (inclusive or)
+
+// Short circuit logic
+if (isOlderThan18 || (isOlderThan8 && isAtLeast140Cm)) {
+    alert("You can board the ride!");
+} else {
+    alert("Go home");
+}
