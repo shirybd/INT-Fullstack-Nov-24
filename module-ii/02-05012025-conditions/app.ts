d32cbb13ee4255b3d0e1818c68d75a09914936e1
Omer Dolev <omerdolev90@gmail.com>
2025-01-05 18:33:24 +0200

    start validate right triangle

---
+++ module-ii/02-05012025-conditions/app.ts
@@ -0,0 +1,7 @@
+alert("let's check if your triangle is a valid right triangle");
+
+const side1 = Number(prompt("Please enter first side length"));
+const side2 = Number(prompt("Please enter second side length"));
+const hypotenous = Number(prompt("Please enter hypotenous length"));
+
+
---
+++ module-ii/02-05012025-conditions/dist/app.js
@@ -0,0 +1,4 @@
+alert("let's check if your triangle is a valid right triangle");
+var side1 = Number(prompt("Please enter first side length"));
+var side2 = Number(prompt("Please enter second side length"));
+var hypotenous = Number(prompt("Please enter hypotenous length"));
