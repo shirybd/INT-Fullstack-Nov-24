f1264eace59e2a677d6e69974a3b1c35707925cf
Omer Dolev <omerdolev90@gmail.com>
2025-01-05 19:28:18 +0200

    booleans and if

--- module-ii/02-05012025-conditions/app.ts
+++ module-ii/02-05012025-conditions/app.ts
@@ -4,4 +4,26 @@ const side1 = Number(prompt("Please enter first side length"));
 const side2 = Number(prompt("Please enter second side length"));
 const hypotenous = Number(prompt("Please enter hypotenous length"));
 
+const realHypotenous = side1 ** 2 + side2 ** 2;
+const isValidRightTriangle = hypotenous ** 2 === realHypotenous;
 
+// compare 2 values from any type
+// === - strict equals
+// !== - strict not equals
+
+// compare 2 numbers or 2 strings
+// > - greater than
+// < - lower than
+// >= - greater or equals
+// <= - lower or equals
+
+// negate a boolean
+// ! - negate a value
+
+if (isValidRightTriangle) {
+    alert("it's a valid right triangle");
+}
+
+if (!isValidRightTriangle) {
+    alert("it's NOT a valid right triangle");
+}
--- module-ii/02-05012025-conditions/dist/app.js
+++ module-ii/02-05012025-conditions/dist/app.js
@@ -2,3 +2,21 @@ alert("let's check if your triangle is a valid right triangle");
 var side1 = Number(prompt("Please enter first side length"));
 var side2 = Number(prompt("Please enter second side length"));
 var hypotenous = Number(prompt("Please enter hypotenous length"));
+var realHypotenous = Math.pow(side1, 2) + Math.pow(side2, 2);
+var isValidRightTriangle = Math.pow(hypotenous, 2) === realHypotenous;
+// compare 2 values from any type
+// === - strict equals
+// !== - strict not equals
+// compare 2 numbers or 2 strings
+// > - greater than
+// < - lower than
+// >= - greater or equals
+// <= - lower or equals
+// negate a boolean
+// ! - negate a value
+if (isValidRightTriangle) {
+    alert("it's a valid right triangle");
+}
+if (!isValidRightTriangle) {
+    alert("it's NOT a valid right triangle");
+}
