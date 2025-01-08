e360c65f0e87bd64b9791ed5f35c76493141976e
Omer Dolev <omerdolev90@gmail.com>
2025-01-08 21:01:46 +0200

    min

--- module-ii/03-08012025-loops/dist/min.js
+++ module-ii/03-08012025-loops/dist/min.js
@@ -1,1 +1,10 @@
-var nextNumber = prompt('Please enter a number (write "stop" to finish input)');
+var min = Infinity;
+var userInput = prompt('Please enter a number (write "stop" to finish input)');
+while (userInput !== "stop") {
+    var nextNumber = Number(userInput);
+    if (nextNumber < min) {
+        min = nextNumber;
+    }
+    userInput = prompt('Please enter a number (write "stop" to finish input)');
+}
+alert(min);
--- module-ii/03-08012025-loops/min.ts
+++ module-ii/03-08012025-loops/min.ts
@@ -1,1 +1,14 @@
-const nextNumber = prompt('Please enter a number (write "stop" to finish input)');
+let min = Infinity;
+let userInput = prompt('Please enter a number (write "stop" to finish input)');
+
+while (userInput !== "stop") {
+    const nextNumber = Number(userInput);
+    
+    if (nextNumber < min) {
+        min = nextNumber;
+    }
+
+    userInput = prompt('Please enter a number (write "stop" to finish input)');
+}
+
+alert(min);
