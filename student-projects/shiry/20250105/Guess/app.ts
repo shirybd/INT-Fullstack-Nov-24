f6f44cbb763f8deb7a5ac38690ecd01746aec7e9
Shiry Bar Dayan <shirybd@gmail.com>
2025-01-08 11:46:26 +0200

    Improved guess

--- student-projects/shiry/20250105/Guess/app.ts
+++ student-projects/shiry/20250105/Guess/app.ts
@@ -5,22 +5,40 @@ Create a guess my number game
     * The computer tells the user if their guess was right
     * Display proper error message when the input is not a valid number
     (Single guess)
+
+    4. Improve our guess my number game
+    * Allow the user 3 guesses
+    * After each guess, if the user got it wrong, tell the user if his number was above or bellow the secret number
+    * Stop the program on invalid input
 */
-const randNumber : number = Math.floor(Math.random() * 10);
-const UserNumber : number = Number(prompt("try to guess my number"));
+const randNumber: number = Math.floor(Math.random() * 10);
+let UserNumber: number;
+let index: number = 3;
 
-if (!isNaN(UserNumber) )
-{
-    if(UserNumber===randNumber)
-    {
-        alert("you Got it !!!");
-    }
-    else
-    {
-        alert("No, that's not my number. my number was " + randNumber);
+
+while (index > 0) {
+    UserNumber =  Number(prompt("try to guess my number. between 0 and 9"));
+    if (!isNaN(UserNumber)) {
+        if (UserNumber === randNumber) {
+            alert("you Got it !!!");
+            break;
+        }
+        else if (randNumber>UserNumber) {
+            alert("No, that's not my number. Try a bigger number" );
+        }
+        else {
+            alert("No, that's not my number. Try a smaller number" );
+        }
+
     }
 
+    else {
+        alert("you didn't enter a number");
+        break;
+    }
+
+    index--;
 }
-else {
-    alert("you didn't enter a number");
+if (index === 0) {
+    alert("My number was " + randNumber );
 }
--- student-projects/shiry/20250105/Guess/dist/app.js
+++ student-projects/shiry/20250105/Guess/dist/app.js
@@ -5,17 +5,35 @@ Create a guess my number game
     * The computer tells the user if their guess was right
     * Display proper error message when the input is not a valid number
     (Single guess)
+
+    4. Improve our guess my number game
+    * Allow the user 3 guesses
+    * After each guess, if the user got it wrong, tell the user if his number was above or bellow the secret number
+    * Stop the program on invalid input
 */
 var randNumber = Math.floor(Math.random() * 10);
-var UserNumber = Number(prompt("try to guess my number"));
-if (!isNaN(UserNumber)) {
-    if (UserNumber === randNumber) {
-        alert("you Got it !!!");
+var UserNumber;
+var index = 3;
+while (index > 0) {
+    UserNumber = Number(prompt("try to guess my number. between 0 and 9"));
+    if (!isNaN(UserNumber)) {
+        if (UserNumber === randNumber) {
+            alert("you Got it !!!");
+            break;
+        }
+        else if (randNumber > UserNumber) {
+            alert("No, that's not my number. Try a bigger number");
+        }
+        else {
+            alert("No, that's not my number. Try a smaller number");
+        }
     }
     else {
-        alert("No, that's not my number. my number was " + randNumber);
+        alert("you didn't enter a number");
+        break;
     }
+    index--;
 }
-else {
-    alert("you didn't enter a number");
+if (index === 0) {
+    alert("My number was " + randNumber);
 }
