221175cfa2068029af4fd6374760d5343bca15d1
Omer Dolev <omerdolev90@gmail.com>
2025-01-08 18:42:56 +0200

    max number

---
+++ module-ii/03-08012025-loops/dist/max.js
@@ -0,0 +1,8 @@
+var firstNumber = Number(prompt("first number"));
+var secondNumber = Number(prompt("second number"));
+if (firstNumber > secondNumber) {
+    alert(firstNumber);
+}
+else {
+    alert(secondNumber);
+}
---
+++ module-ii/03-08012025-loops/max.ts
@@ -0,0 +1,8 @@
+const firstNumber = Number(prompt("first number"));
+const secondNumber = Number(prompt("second number"));
+
+if (firstNumber > secondNumber) {
+    alert(firstNumber);
+} else {
+    alert(secondNumber);
+}
