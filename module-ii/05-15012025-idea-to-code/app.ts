b6e7156d885480435a4c1a4fe581e7106063e523
Omer Dolev <omerdolev90@gmail.com>
2025-01-15 20:03:24 +0200

    skeleton

--- module-ii/05-15012025-idea-to-code/app.ts
+++ module-ii/05-15012025-idea-to-code/app.ts
@@ -2,3 +2,26 @@
 // Locations and navigation - show description of current location and available options
 // End game states - get to certain state/goal to win, or run into "trouble" and loose
 // Inventory - track items in the user's possession and their state, allow using and dropping items, (limit invnetory size?)
+
+playGame();
+
+function playGame() {
+    // loop while still playing
+    // call current room
+    // save next room
+}
+
+function firstRoom() {
+    // show room description
+    // get player action
+    // return second/third/fourth room
+    return secondRoom;
+}
+
+function secondRoom() {
+    return thirdRoom;
+}
+
+function thirdRoom() {
+    return secondRoom;
+}
