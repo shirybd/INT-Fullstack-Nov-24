13229db35c549bd29684a0fc6acdf0cd19775275
Omer Dolev <omerdolev90@gmail.com>
2025-01-15 19:21:28 +0200

    define scope

---
+++ module-ii/05-15012025-idea-to-code/dist/app.js
@@ -0,0 +1,4 @@
+// Text based adventure game
+// Locations and navigation - show description of current location and available options
+// End game states - get to certain state/goal to win, or run into "trouble" and loose
+// Inventory - track items in the user's possession and their state, allow using and dropping items, (limit invnetory size?)
