743a01aebe871bbfd01318180c84507fdf2732c1
Omer Dolev <omerdolev90@gmail.com>
2025-01-19 20:04:39 +0200

    print 10 numbers

---
+++ module-ii/06-19012025/dist/grades.js
@@ -0,0 +1,7 @@
+var numbers = [];
+for (var i = 0; i < 10; i++) {
+    numbers[i] = Number(prompt("Please enter a number"));
+}
+for (var i = 0; i < 10; i++) {
+    alert(numbers[i]);
+}
---
+++ module-ii/06-19012025/grades.ts
@@ -0,0 +1,9 @@
+const numbers: number[] = [];
+
+for (let i = 0; i < 10; i++) {
+    numbers[i] = Number(prompt("Please enter a number"));
+}
+
+for (let i = 0; i < 10; i++) {
+    alert(numbers[i]);
+}
