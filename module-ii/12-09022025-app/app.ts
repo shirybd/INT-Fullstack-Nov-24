c6f30202f2219cf20ea922d02ff4f0b442f82854
Omer Dolev <omerdolev90@gmail.com>
2025-02-09 18:54:09 +0200

    app model and api

--- module-ii/12-09022025-app/app.ts
+++ module-ii/12-09022025-app/app.ts
@@ -10,5 +10,38 @@
 // manage loaned equipment
 
 // Model
+type ConsumableItem = {
+    id: string,
+    name: string,
+    category: "Supplies",
+    description?: string,
+    priceInAgorot: number,
+};
+
+type EquipmentItem = {
+    id: string,
+    name: string,
+    category: "Furniture" | "IT",
+    description?: string,
+    priceInAgorot: number,
+};
+
+type Item = ConsumableItem | EquipmentItem;
+
+type Stock = { itemId: string, quantity: number }[];
+type Inventory = { itemId: string, serial: string }[];
 
 // API
+export let items: Item[] = [];
+export let stock: Stock = [];
+export let inventory: Inventory = [];
+
+export function addItem(item: Item) {}
+
+export function addToStock(itemId: string, amount: number) {}
+
+export function consume(itemId: string, quantity: number) {}
+
+export function acquire(itemId: string, serial: string) {}
+
+export function decomission(serial: string) {}
--- module-ii/12-09022025-app/dist/app.js
+++ module-ii/12-09022025-app/dist/app.js
@@ -3,9 +3,12 @@
 // forms
 // manage array of objects
 // relation to other objects
-// office
-// manage inventory
-// manage employees
-// manage loaned equipment
-// Model
 // API
+export let items = [];
+export let stock = [];
+export let inventory = [];
+export function addItem(item) { }
+export function addToStock(itemId, amount) { }
+export function consume(itemId, quantity) { }
+export function acquire(itemId, serial) { }
+export function decomission(serial) { }
