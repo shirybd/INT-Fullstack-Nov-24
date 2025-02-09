6452051ac8cc570af0a0a55705f96030e8293c40
Omer Dolev <omerdolev90@gmail.com>
2025-02-09 18:31:52 +0200

    app start

---
+++ module-ii/12-09022025-app/app.ts
@@ -0,0 +1,14 @@
+// base requirements:
+// multiple pages
+// forms
+// manage array of objects
+// relation to other objects
+
+// office
+// manage inventory
+// manage employees
+// manage loaned equipment
+
+// Model
+
+// API
---
+++ module-ii/12-09022025-app/dist/app.js
@@ -0,0 +1,11 @@
+// base requirements:
+// multiple pages
+// forms
+// manage array of objects
+// relation to other objects
+// office
+// manage inventory
+// manage employees
+// manage loaned equipment
+// Model
+// API
